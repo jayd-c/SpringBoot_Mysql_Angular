88cf2623e2c99a50b2ee88eb7d7ccce0cad2d5a2
JayanDesilva <jaycdesilva@gmail.com>
2023-09-04 20:16:31 -0400

    Initial commit

---
+++ src/test/java/com/gitrepos/springangularmysql/SpringAngularMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrepos.springangularmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
