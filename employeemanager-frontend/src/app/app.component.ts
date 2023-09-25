a0ca2efcaf6d88bf9e0597fa0977e044c9b5ab17
JayanDesilva <jaycdesilva@gmail.com>
2023-09-24 21:29:08 -0400

    upto Https get front-end can access all employees

--- employeemanager-frontend/src/app/app.component.ts
+++ employeemanager-frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanager-frontend';
+export class AppComponent implements OnInit{
+
+  public employees: Employee[] = [];
+  // employeeService: EmployeeService = inject(EmployeeService);
+  constructor(private employeeService:EmployeeService){}
+
+  // constructor(private employeeService:EmployeeService){} *** this way also we can inject the employee servcie.
+  ngOnInit() {
+      this.getEmployees();
+  }
+
+  public getEmployees():void {
+    this.employeeService.getEmployees().subscribe(
+      (response:Employee[]) => {
+        this.employees  = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  
 }
---
+++ employeemanager-frontend/src/app/employee.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  url = 'http://localhost:8080/employee';
+
+// async getAllEmployees(): Promise<Employee[]> {
+//   const data = await fetch(this.url);
+//   return await data.json() ?? [];
+// }
+
+constructor(private http: HttpClient) {}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.url}/all`);
+  }
+
+  public addEmployee(employee:Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.url}/add`, employee);
+  }
+
+  public updateEmployee(employee:Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.url}/add`, employee);
+  }
+
+  public deleteEmployee(employeeId:number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/delete/${employeeId}`);
+  }
+}
